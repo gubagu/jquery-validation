28f9108519ee59187370eb67d2c778ae6f03544d
Zarro <zlatozar.Dichev@ictu.nl>
2015-03-02 17:52:59 +0100

    Fix ide folder

--- src/additional/bsnNL.js
+++ src/additional/bsnNL.js
@@ -1,16 +1,13 @@
-/**
- * Created by ICTU on 2-3-2015.
- */
-$.validator.addMethod( "bsnNL", function (value) {
-    "use strict";
-    var number = value.length, pos = 0, result = 0, i;
+/** Created by ICTU on 2-3-2015.*/
+$.validator.addMethod( "bsnNL", function( value ) {"use strict";
+    var number = value.length, pos = 0, result = 0, i = number;
 
-    if ( number < 8 || number > 9 || (!isNaN(value) && parseInt(value) ) == false) {
+    if ( number < 8 || number > 9 || (!isNaN(value) && parseInt(value,10) ) == false) {
         return false;
     }
 
-    for (i = number; i > 0; i--) {
-        result += ( i != 1 ) ? ( value.charAt( pos ) * i ) : (value.charAt( pos ) * i * -1);
+    for (i; i > 0; i--) {
+        result += ( i !== 1 ) ? ( value.charAt( pos ) * i ) : (value.charAt( pos ) * i * -1);
         pos++;
     }
 
